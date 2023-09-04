7add30e0f20c9db58bdeb260dc61b4b142bf5ee7
Soumyodeep-Das <linux.programmer.06@gmail.com>
2023-09-05 01:19:18 +0530

    5th Commit on 5th Sep at 01:20

---
+++ insertion_sort_16.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class insertion_sort_16 {
+    public static void insertionSort(int[] array, int arraySize){
+        int current, j;
+        for(int i=1; i<arraySize; i++){
+            current=array[i];
+            for(j=i-1; j>=0 && current<array[j]; j--){
+                array[j+1]=array[j];
+            }
+            array[j+1]=current;
+        } 
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nInsertion Sort");
+        System.out.print("\nEnter array size : ");
+        int arraySize = sc.nextInt();
+        int[] array = new int[arraySize];
+        System.out.println("\nEnter numbers to sort : \n");
+        for(int i=0; i<arraySize; i++){
+            array[i]=sc.nextInt();
+        }
+        insertionSort(array, arraySize);
+        System.out.println("\nThe sorted array is : \n");
+        for(int i=0; i<arraySize; i++){
+            System.out.print(array[i]+"\t");
+        }
+        sc.close();
+    }
+}
---
+++ selection_sort_15.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class selection_sort_15 {
+    public static void selectionSort(int[] array, int arraySize){
+        int minimum, temp;
+        for(int i=0; i<arraySize; i++){
+            minimum=i;
+            for(int j=i; j<arraySize; j++){
+                if(array[minimum]>array[j])
+                    minimum=j;
+            }
+            temp=array[minimum];
+            array[minimum]=array[i];
+            array[i]=temp;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nSelection Sort");
+        System.out.print("\nEnter size of array : ");
+        int arraySize=sc.nextInt();
+        int[] array = new int[arraySize];
+        System.out.println("\nEnter numbers to sort : ");
+        for(int i=0; i<arraySize; i++){
+            array[i]=sc.nextInt();
+        }
+        selectionSort(array, arraySize);
+        System.out.println("\nThe sorted array is : \n");
+        for(int i=0; i<arraySize; i++){
+            System.out.print(array[i]+"\t");
+        }
+        sc.close();
+    }
+}
